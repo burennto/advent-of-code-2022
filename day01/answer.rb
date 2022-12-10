113a5dbd0712361b9da9cbf96fd0d44908176d04
Brent Chuang <421829+burennto@users.noreply.github.com>
2022-12-10 23:49:27 +1000

    Tidy up day 1 with specs

--- day01/answer.rb
+++ day01/answer.rb
@@ -1,6 +1,37 @@
-input = File.read('input.txt')
+require 'minitest/autorun'
 
-elves = input.split("\n\n").map { |items| items.split("\n").map(&:to_i).sum }
+class Day01Test < Minitest::Test
+  def test_example
+    file = 'example.txt'
 
-puts elves.max
-puts elves.max(3).sum
+    assert_equal 24000, solve_part_1(file)
+    assert_equal 45000, solve_part_2(file)
+  end
+
+  def test_input
+    file = 'input.txt'
+
+    assert_equal 70764, solve_part_1(file)
+    assert_equal 203905, solve_part_2(file)
+  end
+end
+
+def parse_elves(file)
+  File.read(file)
+    .split("\n\n")
+    .map do |items|
+      items.split("\n")
+        .map(&:to_i)
+        .sum
+    end
+end
+
+def solve_part_1(file)
+  elves = parse_elves(file)
+  elves.max
+end
+
+def solve_part_2(file)
+  elves = parse_elves(file)
+  elves.max(3).sum
+end
