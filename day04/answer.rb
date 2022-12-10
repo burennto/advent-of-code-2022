b77f6a5df84f79a03a163cae38e7c53cbbd1d491
Brent Chuang <421829+burennto@users.noreply.github.com>
2022-12-11 00:36:10 +1000

    Tidy up day 4 with specs

--- day04/answer.rb
+++ day04/answer.rb
@@ -1,9 +1,24 @@
+require 'minitest/autorun'
+
+class Day04Test < Minitest::Test
+  def setup
+    @example = 'example.txt'
+    @input = 'input.txt'
+  end
+
+  def test_part_1
+    assert_equal 2, solve_part_1(@example)
+    assert_equal 431, solve_part_1(@input)
+  end
+
+  def test_part_2
+    assert_equal 4, solve_part_2(@example)
+    assert_equal 823, solve_part_2(@input)
+  end
+end
+
 class Elf
   attr_reader :start, :finish
-
-  def self.parse(line)
-    line.chomp.split(',').map { |string| Elf.new(*string.split('-').map(&:to_i)) }
-  end
 
   def initialize(start, finish)
     @start, @finish = start, finish
@@ -24,12 +39,26 @@ class Elf
   end
 end
 
-puts File.foreach('input.txt').with_object([]).each { |line, pairs|
-  elf_1, elf_2 = Elf.parse(line)
-  pairs << true if elf_1.contains?(elf_2) || elf_2.contains?(elf_1)
-}.count
+def parse_elf_pairs(file)
+  File.readlines(file)
+    .map(&:chomp)
+    .map do |line|
+      line.split(',').map { |string| Elf.new(*string.split('-').map(&:to_i)) }
+    end
+end
 
-puts File.foreach('input.txt').with_object([]).each { |line, pairs|
-  elf_1, elf_2 = Elf.parse(line)
-  pairs << true if elf_1.overlaps?(elf_2) || elf_2.overlaps?(elf_1)
-}.count
+def solve_part_1(file)
+  parse_elf_pairs(file).reduce(0) do |count, pairs|
+    elf_1, elf_2 = pairs
+    count += 1 if elf_1.contains?(elf_2) || elf_2.contains?(elf_1)
+    count
+  end
+end
+
+def solve_part_2(file)
+  parse_elf_pairs(file).reduce(0) do |count, pairs|
+    elf_1, elf_2 = pairs
+    count += 1 if elf_1.overlaps?(elf_2) || elf_2.overlaps?(elf_1)
+    count
+  end
+end
